83b5327aa204d199e428a30fc52c9a277fa44040
Elvira Wiklund <wiklundel@gmail.com>
2026-05-14 15:03:48 +0200

    firebase config

---
+++ HitsterApp/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDSAlmdpJ7vS_JgN6EPAulVILZxoKeZ-nA",
+  authDomain: "hitsterapp-1902d.firebaseapp.com",
+  projectId: "hitsterapp-1902d",
+  storageBucket: "hitsterapp-1902d.firebasestorage.app",
+  messagingSenderId: "31402365072",
+  appId: "1:31402365072:web:4389847be2edcd9b510815",
+  measurementId: "G-9YFH34150V",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
