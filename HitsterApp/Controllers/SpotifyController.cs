2d52762ca71714509b3890b6f7895d8f9950bd2a
Elvira Wiklund <wiklundel@gmail.com>
2026-05-20 11:29:07 +0200

    Lagt till OAuth

--- HitsterApp/Controllers/SpotifyController.cs
+++ HitsterApp/Controllers/SpotifyController.cs
@@ -8,11 +8,13 @@ public class SpotifyController : Controller
 {
     private readonly SpotifyService _spotifyService;
     private readonly MusicCardService _musicCardService;
+    private readonly IConfiguration _configuration;
 
-    public SpotifyController(SpotifyService spotifyService, MusicCardService musicCardService)
+    public SpotifyController(SpotifyService spotifyService, MusicCardService musicCardService, IConfiguration configuration)
     {
         _spotifyService = spotifyService;
         _musicCardService = musicCardService;
+        _configuration = configuration;
     }
 
     public async Task<IActionResult> Test()
@@ -20,6 +22,46 @@ public class SpotifyController : Controller
         string token = await _spotifyService.GetAccessToken();
 
         return Content(token);
+    }
+
+    public IActionResult Login()
+    {
+        string clientId = _configuration["Spotify:ClientId"!];
+        string redirectUri = "http://130.239.190.149:5290/Spotify/Callback";
+
+        string scopes = "playlist-read-private playlist-read-collaborative";
+
+        string url = 
+            "https://accounts.spotify.com/authorize" +
+            "?response_type=code" +
+            $"&client_id={clientId}" +
+            $"&scope={Uri.EscapeDataString(scopes)}" +
+            $"redirect_uri={Uri.EscapeDataString(redirectUri)}";
+
+        return Redirect(url);
+    }
+
+    public async Task<IActionResult> ImportPlaylist(string playlistId)
+    {
+        string? accessToken = HttpContext.Session.GetString("SpotifyAccessToken");
+
+        if (accessToken == null)
+        {
+            return RedirectToAction("Login");
+        }
+
+        List<MusicCard> cards = await _spotifyService.GetPlaylistTracks(accessToken, playlistId);
+
+        return Json(cards);
+    }
+
+    public async Task<IActionResult> Callback(string code)
+    {
+        string accessToken = await _spotifyService.GetUserAccessToken(code);
+
+        HttpContext.Session.SetString("SpotifyAccessToken", accessToken);
+
+        return RedirectToAction("ImportPlaylist");
     }
 
     public async Task<IActionResult> Search(string q)
--- HitsterApp/Services/SpotifyService.cs
+++ HitsterApp/Services/SpotifyService.cs
@@ -1,6 +1,5 @@
 using System.Text;
 using System.Text.Json;
-using Google.Cloud.Firestore.V1;
 using HitsterApp.Models;
 
 namespace HitsterApp.Services;
@@ -62,59 +61,117 @@ public class SpotifyService
             ?? throw new Exception("Spotify response did not contain access_token");
     }
 
-    /*
-    public async Task<MusicCard> GetRandomTrackFromSearch()
+    public async Task<string> GetUserAccessToken(string code)
     {
-        string token = await GetAccessToken();
+        string clientId = _configuration["Spotify:ClientId"]!;
+        string clientSecret = _configuration["Spotify:ClientSecret"]!;
+        string redirectUri = "http://130.239.190.149:5290/Spotify/Callback";
+
+        string credentials = Convert.ToBase64String(
+            Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}")
+        );
 
         _httpClient.DefaultRequestHeaders.Clear();
-        _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+        _httpClient.DefaultRequestHeaders.Add("Authorization", $"Basic {credentials}");
 
-        int randomYear = Random.Shared.Next(1980, 2025);
+        var body = new FormUrlEncodedContent(new Dictionary<string, string>
+        {
+            { "grant_type", "authorization_code" },
+            { "code", code },
+            { "redirect_uri", redirectUri }
+        });
 
-        var response = await _httpClient.GetAsync(
-            $"https://api.spotify.com/v1/search?q=year:{randomYear}&type=track&market=SE&limit=50"
+        var response = await _httpClient.PostAsync(
+            "https://accounts.spotify.com/api/token",
+            body
         );
 
         var json = await response.Content.ReadAsStringAsync();
 
         if (!response.IsSuccessStatusCode)
         {
-            throw new Exception($"Spotify search error: {json}");
+            throw new Exception("Spotify OAuth error: " + json);
         }
 
         using JsonDocument doc = JsonDocument.Parse(json);
 
-        var tracks = doc.RootElement
-            .GetProperty("tracks")
+        return doc.RootElement
+            .GetProperty("access_token")
+            .GetString()
+            ?? throw new Exception("Spotify OAuth response did not contain access_token");
+    }
+
+    public async Task<List<MusicCard>> GetPlaylistTracks(string accessToken, string playlistId)
+    {
+         _httpClient.DefaultRequestHeaders.Clear();
+        _httpClient.DefaultRequestHeaders.Add(
+            "Authorization",
+            $"Bearer {accessToken}"
+        );
+
+        var response = await _httpClient.GetAsync(
+            $"https://api.spotify.com/v1/playlists/{playlistId}/items?market=SE&limit=50"
+        );
+
+        var json = await response.Content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception("Spotify search error: " + json);
+        }
+
+        using JsonDocument doc = JsonDocument.Parse(json);
+
+        var items = doc.RootElement
             .GetProperty("items")
             .EnumerateArray()
             .ToList();
 
-        var track = tracks[Random.Shared.Next(tracks.Count)];
+        List<MusicCard> cards = new();
 
-        string title = track.GetProperty("name").GetString() ?? "";
-
-        string artist = track
-            .GetProperty("artists")[0]
-            .GetProperty("name")
-            .GetString() ?? "";
-
-        string releaseDate = track
-            .GetProperty("album")
-            .GetProperty("release_date")
-            .GetString() ?? "0000";
-
-        int releaseYear = int.Parse(releaseDate.Substring(0, 4));
-
-        return new MusicCard
+        foreach (var item in items)
         {
-            Title = title,
-            Artist = artist,
-            ReleaseYear = releaseYear,
-            State = "deck"
-        };
-    }*/
+            if(!item.TryGetProperty("track", out JsonElement track)
+                || track.ValueKind == JsonValueKind.Null)
+            {
+                continue;
+            }
+
+            string title = track.GetProperty("name").GetString() ?? "";
+            string artist = track
+                .GetProperty("artist")[0]
+                .GetProperty("name")
+                .GetString() ?? "";
+
+            string realeseDate = track
+                .GetProperty("album")
+                .GetProperty("release_date")
+                .GetString() ?? "0000";
+
+            int releaseYear = int.Parse(realeseDate.Substring(0, 4));
+
+            string spotifyId = track.GetProperty("id").GetString() ?? "";
+
+            string previewUrl = "";
+            if (track.TryGetProperty("preview_url", out JsonElement previewElement)
+                && previewElement.ValueKind != JsonValueKind.Null)
+            {
+                previewUrl = previewElement.GetString() ?? "";
+            }
+
+            cards.Add(new MusicCard
+            {
+                Title = title,
+                Artist = artist,
+                ReleaseYear = releaseYear,
+                SpotifyId = spotifyId,
+                PreviewUrl = previewUrl,
+                State = "deck"
+            });
+        }
+
+        return cards;
+    }
 
     public async Task<MusicCard> SearchTrack(string search)
     {
